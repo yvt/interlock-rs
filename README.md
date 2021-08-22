73706cdf9ffa640c4823a0045aa0160b6e6a06a3
yvt <i@yvt.jp>
2021-08-22 21:05:56 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# interlock
+
+*Work in progress*
+
+Readers-writer locks optimized for interval locks.
---
+++ src/lib.rs
@@ -0,0 +1,9 @@
+#![doc = include_str!("../README.md")]
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
