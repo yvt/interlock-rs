2d7944a117b1db11cca6b7ae18392add7b3574dc
yvt <i@yvt.jp>
2021-08-29 16:17:31 +0900

    refactor(utils): externalize `crate::utils::pin::tests`

--- src/utils/pin.rs
+++ src/utils/pin.rs
@@ -7,6 +7,9 @@ use core::{
 };
 use pin_utils::pin_mut;
 
+#[cfg(test)]
+mod tests;
+
 pub trait EarlyDrop {
     /// Early-destructor. Called on the referent when a [`EarlyDropGuard`] is
     /// dropped.
@@ -156,50 +159,5 @@ impl<T: fmt::Debug> fmt::Debug for EarlyDropGuard<T> {
         } else {
             f.write_str("< uninitialized >")
         }
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    extern crate std;
-    use std::{cell::Cell, prelude::v1::*};
-
-    #[test]
-    fn early_drop_called() {
-        struct DropCheck<'a>(&'a Cell<usize>, &'a mut bool);
-
-        impl EarlyDrop for DropCheck<'_> {
-            unsafe fn early_drop(self: Pin<&Self>) {
-                assert_eq!(self.0.get(), 1, "early_drop was called too early");
-                self.0.set(2);
-            }
-        }
-
-        impl Drop for DropCheck<'_> {
-            fn drop(&mut self) {
-                assert_eq!(self.0.get(), 2, "early_drop wasn't called");
-                self.0.set(3);
-
-                assert_eq!(*self.1, false, "drop was called twice");
-                *self.1 = true;
-            }
-        }
-
-        let step = Cell::new(0);
-        let mut dropped = false;
-        let mut edg = Box::pin(EarlyDropGuard::new());
-
-        Pin::as_mut(&mut edg).get_or_insert_with(|| DropCheck(&step, &mut dropped));
-        Pin::as_mut(&mut edg)
-            .get_or_insert_with(|| unreachable!())
-            .0
-            .set(1);
-
-        drop(edg);
-
-        assert_eq!(step.get(), 3, "drop wasn't called");
-        assert_eq!(dropped, true, "drop wasn't called");
     }
 }
